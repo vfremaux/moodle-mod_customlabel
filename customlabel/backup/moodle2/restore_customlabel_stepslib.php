12928616979d2fa9aa77fd3f38072422b9144ba6
Valery Fremaux <valery.fremaux@gmail.com>
2012-12-16 17:39:42 +0100

    fix backup restore

--- customlabel/backup/moodle2/restore_customlabel_stepslib.php
+++ customlabel/backup/moodle2/restore_customlabel_stepslib.php
@@ -53,7 +53,8 @@ class restore_customlabel_activity_structure_step extends restore_activity_struc
     
     
     protected function process_customlabel($data){
-
+		global $DB;
+		
         $data = (object)$data;        
         $oldid = $data->id;
         $data->course = $this->get_courseid();
@@ -64,22 +65,45 @@ class restore_customlabel_activity_structure_step extends restore_activity_struc
         $this->apply_activity_instance($newitemid);
     }
 
-    protected function process_customlabel_metadatavalue($data) {
+    protected function process_metadatatype($data) {
     	global $DB;
     	
         $data = (object)$data;        
         $oldid = $data->id;
 
-        $data->typeid = $this->get_mappingid('customlabel_mtd_type', $data->typeid);
-
         $data->timemodified = $this->apply_date_offset($data->timemodified);
 
         // The data is actually inserted into the database later in inform_new_usage_id.
-        $newitemid = $DB->insert_record('customlabel_mtd_value', $data);
-        $this->set_mapping('customlabel_mtd_value', $oldid, $newitemid, false); // Has no related files
+        // items with similar code SHOULD NOT be duplicated. Old value will prepend to preserve local system stability.
+        if ($oldrecord = $DB->get_record('customlabel_mtd_type', 'code', $data->code)){
+        	$itemid = $oldrecord->id;
+        } else {
+        	$itemid = $DB->insert_record('customlabel_mtd_type', $data);
+        }
+        $this->set_mapping('customlabel_mtd_type', $oldid, $itemid, false); // Has no related files
     }
 
-    protected function process_customlabel_metadataconstraint($data) {
+    protected function process_metadatavalue($data) {
+    	global $DB;
+    	
+        $data = (object)$data;        
+        $oldid = $data->id;
+
+		// If an older record matches the same code, use the local one.
+        if ($oldrecord = $DB->get_record('customlabel_mtd_value', 'code', $data->code)){
+        	$itemid = $oldrecord->id;
+        } else {
+
+	        $data->typeid = $this->get_mappingid('customlabel_mtd_type', $data->typeid);
+	        $data->timemodified = $this->apply_date_offset($data->timemodified);
+
+	        // The data is actually inserted into the database later in inform_new_usage_id.
+	        $itemid = $DB->insert_record('customlabel_mtd_value', $data);
+	    }
+        $this->set_mapping('customlabel_mtd_value', $oldid, $itemid, false); // Has no related files
+    }
+
+    protected function process_metadataconstraint($data) {
     	global $DB;
     	
         $data = (object)$data;        
@@ -88,14 +112,17 @@ class restore_customlabel_activity_structure_step extends restore_activity_struc
         $data->value1 = $this->get_mappingid('customlabel_mtd_value', $data->value1);
         $data->value2 = $this->get_mappingid('customlabel_mtd_value', $data->value2);
 
-        $data->timemodified = $this->apply_date_offset($data->timemodified);
-
-        // The data is actually inserted into the database later in inform_new_usage_id.
-        $newitemid = $DB->insert_record('customlabel_mtd_value', $data);
-        $this->set_mapping('customlabel_mtd_onstraint', $oldid, $newitemid, false); // Has no related files
+		if ($oldconstraint = $DB->get_record_select('customlabel_mtd_constraint', " (value1 = $data->value1 AND value2 = $data->value2) OR (value2 = $data->value1 AND value1 = $data->value2) ")){
+			$itemid = $oldconstraint->id;
+		} else {	
+	        $data->timemodified = $this->apply_date_offset($data->timemodified);	
+	        // The data is actually inserted into the database later in inform_new_usage_id.
+	        $itemid = $DB->insert_record('customlabel_mtd_value', $data);
+	    }
+        $this->set_mapping('customlabel_mtd_constraint', $oldid, $itemid, false); // Has no related files
     }
 
-    protected function process_customlabel_metadatacoursedatum($data) {
+    protected function process_metadatacourse($data) {
     	global $DB;
     	
         $data = (object)$data;
