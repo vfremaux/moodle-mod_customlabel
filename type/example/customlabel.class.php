42d3496a665e7e44813e16897d7c214ce8585913
valery <valery.fremaux@gmail.com>
2017-03-08 15:16:09 +0100

    Fix element types using textarea to editor

--- type/example/customlabel.class.php
+++ type/example/customlabel.class.php
@@ -1,4 +1,27 @@
 <?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * @package    mod_customlabel
+ * @category   mod
+ * @author     Valery Fremaux <valery.fremaux@club-internet.fr>
+ * @copyright  (C) 2008 onwards Valery Fremaux (http://www.mylearningfactory.com)
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL
+ */
+defined('MOODLE_INTERNAL') || die();
 
 require_once ($CFG->dirroot."/mod/customlabel/type/customtype.class.php");
 
@@ -16,7 +39,7 @@ class customlabel_type_example extends customlabel_type{
 
         $field = new StdClass;
         $field->name = 'example';
-        $field->type = 'textarea';
+        $field->type = 'editor';
         $field->itemid = 0;
         $field->rows = 20;
         $this->fields['example'] = $field;
