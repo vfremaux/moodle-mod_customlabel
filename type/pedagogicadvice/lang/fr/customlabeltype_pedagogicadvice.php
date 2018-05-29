b11508007ec3fcf0c42c8cd2d60c2b0593d84829
valery <valery.fremaux@gmail.com>
2018-05-29 19:02:51 +0200

    add availability handling on standard course formats

--- type/pedagogicadvice/lang/fr/customlabeltype_pedagogicadvice.php
+++ type/pedagogicadvice/lang/fr/customlabeltype_pedagogicadvice.php
@@ -33,16 +33,18 @@ $string['advice'] = 'Conseil&nbsp;';
 $string['initiallyvisible'] = 'Visible au chargement de la page&nbsp;';
 
 $string['template'] = '
-<div class="custombox-control pedagogicadvice"><a href="javascript:togglecustom(\'<%%customid%%>\', \'<%%wwwroot%%>\')"><img id="customctl<%%customid%%>" src="<%%initialcontrolimage%%>" /></a>
-<span class="custombox-header-caption pedagogicadvice"><b>Note pédagogique</b></span></div>
+<div class="custombox-control pedagogicadvice">
+    <a href="javascript:togglecustom(\'<%%customid%%>\', \'<%%wwwroot%%>\')"><img id="customctl<%%customid%%>" src="<%%initialcontrolimage%%>" /></a>
+    <span class="custombox-header-caption pedagogicadvice"><b>Note pédagogique</b></span>
+</div>
 <div class="custombox-content pedagogicadvice" id="custom<%%customid%%>">
-<p class="custombox-helper pedagogicadvice"><b>(Cette note ne peut être vue que par les enseignants)</b></p>
-<table width="100%" class="custombox-pedagogicadvice">
-    <tr valign="top">
-        <td class="custombox-thumb pedagogicadvice" style="background-image : url(<%%icon%%>);" width="2%"></td>
-        <td class="custombox-content pedagogicadvice"><%%advice%%></td>
-    </tr>
-</table>
+    <p class="custombox-helper pedagogicadvice"><b>(Cette note ne peut être vue que par les enseignants)</b></p>
+    <table width="100%" class="custombox-pedagogicadvice">
+        <tr valign="top">
+            <td class="custombox-thumb pedagogicadvice" style="background-image : url(<%%icon%%>);" width="2%"></td>
+            <td class="custombox-content pedagogicadvice"><%%advice%%></td>
+        </tr>
+    </table>
 </div>
 <script type="text/javascript">
 setupcustom(\'<%%customid%%>\', \'<%%initiallyvisible%%>\', \'<%%wwwroot%%>\');
