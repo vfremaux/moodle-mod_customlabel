e6acb198983c53a7b808ca691b57c984dd7124cc
valery <valery.fremaux@gmail.com>
2019-02-12 12:26:06 +0100

    resync to 3.5

---
+++ type/question/ajax/service.php
@@ -0,0 +1,77 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+require('../../../../../config.php');
+require_once($CFG->dirroot.'/lib/completionlib.php');
+require_once($CFG->dirroot.'/mod/customlabel/locallib.php');
+
+$action = required_param('what', PARAM_TEXT);
+
+require_login();
+
+if ($action == 'submit') {
+    $cmid = required_param('cmid', PARAM_INT);
+    $givenanswer = required_param('answernum', PARAM_INT);
+
+    $cm = $DB->get_record('course_modules', array('id' => $cmid));
+    $instance = $DB->get_record('customlabel', array('id' => $cm->instance));
+
+    $instance->coursemodule = $cm->id;
+    $customlabel = customlabel_load_class($instance);
+
+    $params = array('userid' => $USER->id, 'customlabelid' => $instance->id);
+
+    $correctanswer = $customlabel->get_data('correctanswer');
+    $status = CUSTOMLABEL_QUESTION_ANSWERED;
+    if ($correctanswer == $givenanswer) {
+        $status = CUSTOMLABEL_QUESTION_ANSWER_IS_CORRECT;
+    }
+
+    $ud = $DB->get_record('customlabel_user_data', $params);
+    if (!$ud) {
+        $ud = new StdClass;
+        $ud->userid = $USER->id;
+        $ud->customlabelid = $cm->instance;
+        $ud->completion1 = $status;
+        $ud->completion2 = $answernum;
+        $ud->completion3 = 1; // Number of tries.
+        $ud->timecompleted1 = time();
+        $ud->id = $DB->insert_record('customlabel_user_data', $ud);
+    } else {
+        $ud->completion1 = $status;
+        $ud->completion2 = $answernum;
+        $ud->completion3 += 1; // Number of tries.
+        $DB->update_record('customlabel_user_data', $ud);
+    }
+
+    // Mark completion in moodle completion if needed.
+    $course = $DB->get_record('course', array('id' => $cm->course));
+    $completion = new completion_info($course);
+    if (($completion->is_enabled($cm) == COMPLETION_TRACKING_AUTOMATIC) &&
+            $instance->completion1enabled) {
+        // Unconditionnaly mark. The user has answered.
+        $completion->update_state($cm, COMPLETION_COMPLETE, $USER->id);
+    }
+
+    if (($completion->is_enabled($cm) == COMPLETION_TRACKING_AUTOMATIC) &&
+            $instance->completion2enabled) {
+        // Mark only if correct. The user has answered.
+        if ($status == $status = CUSTOMLABEL_QUESTION_ANSWER_IS_CORRECT) {
+            $completion->update_state($cm, COMPLETION_COMPLETE, $USER->id);
+        }
+    }
+}
+
--- type/question/lang/en/customlabeltype_question.php
+++ type/question/lang/en/customlabeltype_question.php
@@ -41,48 +41,12 @@ $string['notavailableyet'] = 'Not yet available';
 $string['initiallyvisible'] = 'Answer initially visible';
 $string['hint'] = 'Hint';
 $string['hintinitiallyvisible'] = 'Hint initially visible';
+$string['isqcmchallenge'] = 'Is a QCM question challenge';
+$string['shuffleanswers'] = 'Shuffle answers';
+$string['attempts'] = 'Number of attempts before locking';
+$string['correctanswer'] = 'Correct answer (linenumber)';
+$string['completion1'] = 'Student has answed the question.';
+$string['completion2'] = 'Student has answed the question and has good answer.';
+$string['submitqcm'] = 'Submit your answer';
 
 $string['family'] = 'pedagogic';
-
-$string['template'] = '
-<table class="custombox-question" cellspacing="0" width="100%">
-<tr valign="top">
-    <td class="custombox-header-thumb question" style="background-image : url(<%%icon%%>);" width="2%" rowspan="6">
-    </td>
-    <td class="custombox-header-caption question" width="96%">
-        Question !
-    </td>
-</tr>
-<tr valign="top">
-    <td class="custombox-content questiontext">
-        <%%questiontext%%>
-    </td>
-</tr>
-<%if %%hint%% %>
-<tr>
-    <td class="custombox-header-collapser question" align="left" >
-        <a href="javascript:togglecustom(\'<%%customid%%>hint\', \'<%%wwwroot%%>\')"><img id="customctl<%%customid%%>hint" src="<%%hintinitialcontrolimage%%>" /></a> Hint
-    </td>
-</tr>
-<tr valign="top" id="custom<%%customid%%>hint">
-    <td class="custombox-content hint">
-        <%%hint%%>
-    </td>
-</tr>
-<%endif %>
-<tr>
-    <td class="custombox-header-collapser question" align="left">
-        <a href="javascript:togglecustom(\'<%%customid%%>\', \'<%%wwwroot%%>\')"><img id="customctl<%%customid%%>" src="<%%initialcontrolimage%%>" /></a> Solution
-    </td>
-</tr>
-<tr valign="top" id="custom<%%customid%%>">
-    <td class="custombox-content questiontext">
-        <%%answertext%%>
-    </td>
-</tr>
-</table>
-<script type="text/javascript">
-setupcustom(\'<%%customid%%>\', \'<%%initiallyvisible%%>\', \'<%%wwwroot%%>\');
-setupcustom(\'<%%customid%%>hint\', \'<%%hintinitiallyvisible%%>\', \'<%%wwwroot%%>\');
-</script>
-';
