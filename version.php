63046c1cd1f3c4492db0b121e7b436d9942d510f
valery <valery.fremaux@gmail.com>
2020-07-31 09:22:30 +0200

    Automated sync from SVN

--- version.php
+++ version.php
@@ -27,13 +27,13 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version  = 2020063000; // The current module version (Date: YYYYMMDDXX).
-$plugin->requires = 2019111200;
+$plugin->requires = 2020060900;
 $plugin->component = 'mod_customlabel'; // Full name of the plugin (used for diagnostics).
-$plugin->release = '3.8.0 (Build 2020063000)';
+$plugin->release = '3.9.0 (Build 2020063000)';
 $plugin->maturity = MATURITY_STABLE;
 
 // Non moodle attributes.
-$plugin->codeincrement = '3.8.0015';
+$plugin->codeincrement = '3.9.0015';
 $plugin->privacy = 'dualrelease';
 $plugin->prolocations = array(
     'type/coursedata',
